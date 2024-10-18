f0e92e97e1322f651dffb332416eac7816b4b1de
BrandonFVertex <brandon.ferreiras@stu.vertexacademies.org>
2024-10-18 14:08:39 -0400

    Add files via upload

---
+++ Brandon_Ferreiras_JS_03.js
@@ -0,0 +1,23 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+
+// 3. Declare a variable called `age`:
+
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+
+// 5. Assign the `age` variable to a number:
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+let firstName = "Brandon"
+let lastName = "Ferreiras"
+let age = 16 
+console.log(firstName + lastName + age)
